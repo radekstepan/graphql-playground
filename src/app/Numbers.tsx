2d39b219260517967d889f0a9aa20989c0f9554b
Radek Stepan <dev@radekstepan.com>
2022-11-19 17:46:31 -0800

    feat: in style

--- src/app/Numbers.tsx
+++ src/app/Numbers.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import {useMutation} from '@apollo/client';
 import gql from 'graphql-tag';
 
+const css = (classes: unknown[]) => classes.filter(Boolean).join(' ');
+
 const SAVE_NUMBERS = gql`
   mutation SaveNumbers($input: String!) {
     saveNumbers(input: $input) {
@@ -15,15 +17,18 @@ const SAVE_NUMBERS = gql`
 function Numbers() {
   const [input, setInput] = useState('');
 
-  const [saveNumbers] = useMutation(SAVE_NUMBERS, {
+  const [saveNumbers, {data}] = useMutation(SAVE_NUMBERS, {
     variables: {
       input
     }
   });
 
+  const error = !data?.saveNumbers.length;
+
   return (
     <input
       type="text"
+      className={css(['input', data && error && 'status-error'])}
       value={input}
       onChange={({currentTarget}) => {
         setInput(currentTarget.value);
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {createRoot} from 'react-dom/client';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import App from './App';
+import './styles.less'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
