3544af118e26871cab2d498dfafaa8ee83515ed6
Radek Stepan <dev@radekstepan.com>
2022-11-22 22:41:58 -0800

    chore: unused

--- packages/client/src/playground/urql/First.tsx
+++ packages/client/src/playground/urql/First.tsx
@@ -2,15 +2,13 @@ import {useQuery} from 'urql';
 import {GET_NUMBER} from '../../gql';
 
 function First() {
-  const [{data}] = useQuery({
+  useQuery({
     query: GET_NUMBER,
     variables: {
       id: "0"
     },
     requestPolicy: 'cache-first',
   });
-
-  console.log(data?.number?.value);
 
   return null;
 };
